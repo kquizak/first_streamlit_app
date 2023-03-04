73b973f1bb2650a2c007ad0123665ab1b00020c5
kquizak <pop.florin.nicolae@gmail.com>
2023-03-04 15:27:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+import snowflake.connector
 
 streamlit.title("My Parents New Healthy Diner")
 
